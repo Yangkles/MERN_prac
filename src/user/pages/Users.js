018f6ce1c338ae8d481eb53b3275e71cae82887c
Yunho Yang <yangkles@naver.com>
2025-01-11 23:08:47 +0900

    UsersList, UserItem

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,18 @@
 import React from "react";
+import UsersList from "../../shared/components/UsersList";
 
 const Users = () => {
-    return <h2>Users Works!</h2>
+    const USERS = [
+        // {
+        //     id: "u1", 
+        //     name: "Yunho", 
+        //     image: "https://picsum.photos/200/300",
+        //     places: 3,
+        // }
+    ];
+    return (
+        <UsersList items={USERS}/>
+    )
 }
 
 export default Users;
